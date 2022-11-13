d2518e33befc4de3b8c31e6908d13b38da384b2f
proshanto-c <proshantochanda@gmail.com>
2022-11-13 00:21:41 +0000

    Reconfigured hum2midi
    hum2midi now responds to main instead of being a program to be run standalone. This should allow it to be called as a subprocess in another py script.

--- hum2midi.py
+++ hum2midi.py
@@ -14,6 +14,9 @@ import music21
 # THIS PROGRAM USES THE SPICE MODEL TO CONVERT HUM TO PITCH
 model = hub.load("https://tfhub.dev/google/spice/2")
 
+A4 = 440
+C0 = A4 * pow(2, -4.75)
+
 # Function that converts the user-created audio to the format that the model 
 # expects: bitrate 16kHz and only one channel (mono).
 EXPECTED_SAMPLE_RATE = 16000
@@ -22,31 +25,6 @@ def convert_audio_for_model(user_file, output_file='converted_audio_file.wav'):
   audio = audio.set_frame_rate(EXPECTED_SAMPLE_RATE).set_channels(1)
   audio.export(output_file, format="wav")
   return output_file
-
-# Converting to the expected format for the model
-# in all the input 4 input method before, the uploaded file name is at
-# the variable uploaded_file_name
-converted_audio_file = convert_audio_for_model('Mary2.wav')
-
-# Loading audio samples from the wav file:
-sample_rate, audio_samples = wavfile.read(converted_audio_file, 'rb')
-
-# NOW TO ACTUALLY RUN THE SPICE MODEL ON THE AUDIO FILE
-
-# We now feed the audio to the SPICE tf.hub model to obtain pitch and uncertainty outputs as tensors.
-model_output = model.signatures["serving_default"](tf.constant(audio_samples, tf.float32))
-
-pitch_outputs = model_output["pitch"]
-uncertainty_outputs = model_output["uncertainty"]
-
-# 'Uncertainty' basically means the inverse of confidence.
-confidence_outputs = 1.0 - uncertainty_outputs
-
-confidence_outputs = list(confidence_outputs)
-
-pitch_outputs = [float(x) for x in pitch_outputs]
-
-indices = range(len (pitch_outputs))
 
 def output2hz(pitch_output):
   # Constants taken from https://tfhub.dev/google/spice/2
@@ -57,15 +35,6 @@ def output2hz(pitch_output):
   cqt_bin = pitch_output * PT_SLOPE + PT_OFFSET;
   return FMIN * 2.0 ** (1.0 * cqt_bin / BINS_PER_OCTAVE)
 
-pitch_outputs_and_rests = [
-    output2hz(p) if c >= 0.8 else 0
-    for i, p, c in zip(indices, pitch_outputs, confidence_outputs)
-]
-
-A4 = 440
-C0 = A4 * pow(2, -4.75)
-note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-
 def hz2offset(freq):
   # This measures the quantization error for a single note.
   if freq == 0:  # Rests always have zero error.
@@ -74,14 +43,9 @@ def hz2offset(freq):
   h = round(12 * math.log2(freq / C0))
   return 12 * math.log2(freq / C0) - h
 
-
-# The ideal offset is the mean quantization error for all the notes
-# (excluding rests):
-offsets = [hz2offset(p) for p in pitch_outputs_and_rests if p != 0]
-
-ideal_offset = statistics.mean(offsets)
-
 def quantize_predictions(group, ideal_offset):
+  note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
   # Group values are either 0, or a pitch in Hz.
   non_zero_values = [v for v in group if v != 0]
   zero_values_count = len(group) - len(non_zero_values)
@@ -129,47 +93,84 @@ def get_quantization_and_error(pitch_outputs_and_rests, predictions_per_eighth,
 
   return quantization_error, notes_and_rests
 
+def main():
+  # Converting to the expected format for the model
+  # in all the input 4 input method before, the uploaded file name is at
+  # the variable uploaded_file_name
+  converted_audio_file = convert_audio_for_model('Mary2.wav')
 
-best_error = float("inf")
-best_notes_and_rests = None
-best_predictions_per_note = None
+  # Loading audio samples from the wav file:
+  sample_rate, audio_samples = wavfile.read(converted_audio_file, 'rb')
 
-for predictions_per_note in range(20, 65, 1):
-  for prediction_start_offset in range(predictions_per_note):
+  # NOW TO ACTUALLY RUN THE SPICE MODEL ON THE AUDIO FILE
 
-    error, notes_and_rests = get_quantization_and_error(
-        pitch_outputs_and_rests, predictions_per_note,
-        prediction_start_offset, ideal_offset)
+  # We now feed the audio to the SPICE tf.hub model to obtain pitch and uncertainty outputs as tensors.
+  model_output = model.signatures["serving_default"](tf.constant(audio_samples, tf.float32))
 
-    if error < best_error:      
-      best_error = error
-      best_notes_and_rests = notes_and_rests
-      best_predictions_per_note = predictions_per_note
+  pitch_outputs = model_output["pitch"]
+  uncertainty_outputs = model_output["uncertainty"]
 
-# At this point, best_notes_and_rests contains the best quantization.
-# Since we don't need to have rests at the beginning, let's remove these:
-while best_notes_and_rests[0] == 'Rest':
-  best_notes_and_rests = best_notes_and_rests[1:]
-# Also remove silence at the end.
-while best_notes_and_rests[-1] == 'Rest':
-  best_notes_and_rests = best_notes_and_rests[:-1]
+  # 'Uncertainty' basically means the inverse of confidence.
+  confidence_outputs = 1.0 - uncertainty_outputs
+
+  confidence_outputs = list(confidence_outputs)
+
+  pitch_outputs = [float(x) for x in pitch_outputs]
+
+  indices = range(len (pitch_outputs))
+
+  pitch_outputs_and_rests = [
+      output2hz(p) if c >= 0.8 else 0
+      for i, p, c in zip(indices, pitch_outputs, confidence_outputs)
+  ]
+
+  # The ideal offset is the mean quantization error for all the notes
+  # (excluding rests):
+  offsets = [hz2offset(p) for p in pitch_outputs_and_rests if p != 0]
+
+  ideal_offset = statistics.mean(offsets)
 
 
-# Creating the sheet music score.
-sc = music21.stream.Score()
-# Adjust the speed to match the actual singing.
-bpm = 60 * 60 / best_predictions_per_note
-print ('bpm: ', bpm)
-a = music21.tempo.MetronomeMark(number=bpm)
-sc.insert(0,a)
+  best_error = float("inf")
+  best_notes_and_rests = None
+  best_predictions_per_note = None
 
-for snote in best_notes_and_rests:   
-    d = 'half'
-    if snote == 'Rest':      
-      sc.append(music21.note.Rest(type=d))
-    else:
-      sc.append(music21.note.Note(snote, type=d))
+  for predictions_per_note in range(20, 65, 1):
+    for prediction_start_offset in range(predictions_per_note):
 
-# Saving the recognized musical notes as a MIDI file
-converted_audio_file_as_midi = converted_audio_file[:-4] + '.mid'
-fp = sc.write('midi', fp=converted_audio_file_as_midi)
+      error, notes_and_rests = get_quantization_and_error(
+          pitch_outputs_and_rests, predictions_per_note,
+          prediction_start_offset, ideal_offset)
+
+      if error < best_error:      
+        best_error = error
+        best_notes_and_rests = notes_and_rests
+        best_predictions_per_note = predictions_per_note
+
+  # At this point, best_notes_and_rests contains the best quantization.
+  # Since we don't need to have rests at the beginning, let's remove these:
+  while best_notes_and_rests[0] == 'Rest':
+    best_notes_and_rests = best_notes_and_rests[1:]
+  # Also remove silence at the end.
+  while best_notes_and_rests[-1] == 'Rest':
+    best_notes_and_rests = best_notes_and_rests[:-1]
+
+
+  # Creating the sheet music score.
+  sc = music21.stream.Score()
+  # Adjust the speed to match the actual singing.
+  bpm = 60 * 60 / best_predictions_per_note
+  print ('bpm: ', bpm)
+  a = music21.tempo.MetronomeMark(number=bpm)
+  sc.insert(0,a)
+
+  for snote in best_notes_and_rests:   
+      d = 'half'
+      if snote == 'Rest':      
+        sc.append(music21.note.Rest(type=d))
+      else:
+        sc.append(music21.note.Note(snote, type=d))
+
+  # Saving the recognized musical notes as a MIDI file
+  converted_audio_file_as_midi = converted_audio_file[:-4] + '.mid'
+  fp = sc.write('midi', fp=converted_audio_file_as_midi)
